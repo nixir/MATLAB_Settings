e2ed79689dad49a6d26e6eb9f4e7e834943c4ae4
nixir <50483168+nixir@users.noreply.github.com>
2020-02-18 13:45:10 +0900

    Add ref to svg2pdf.m

--- funcs/svg2pdf.m
+++ funcs/svg2pdf.m
@@ -1,8 +1,9 @@
 function [status, msg ] = svg2pdf(fileToConvert)
 % svg2pdf 
 %   REQUIRE INKSCAPE
-%   hoge 0->filetoconvert
+%   hoge -> filetoconvert
 %   hoge.svg -> hoge.pdf
+%   https://jp.mathworks.com/matlabcentral/answers/23511-save-a-figure-to-pdf-or-eps-with-non-standard-fonts#answer_313770
 
 command = strcat('"inkscape" ', {' '}, '"', fileToConvert,'.svg" --export-pdf="', fileToConvert, '.pdf" --export-pdf-version=1.5 --export-area-drawing');
 command = string(command);
